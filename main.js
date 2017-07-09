67fe51e084088a9f5cead7b766df25bbfc37521f
avral <qweasd_96@mail.ru>
2017-07-09 14:59:36 +0300

    tabs to spaces

--- main.js
+++ main.js
@@ -2,46 +2,45 @@ import {ChainStore, FetchChain, Login} from 'graphenejs-lib';
 import {Apis, ChainConfig} from 'graphenejs-ws';
 
 ChainConfig.networks.DACom = {
-	core_asset: 'FLO',
-	address_prefix: 'FLO',
-	chain_id: '526880c720c677ef7b54f964fe68999d1e582a33c8636b0f3b4687d47ae2f67f'
+  core_asset: 'FLO',
+  address_prefix: 'FLO',
+  chain_id: '526880c720c677ef7b54f964fe68999d1e582a33c8636b0f3b4687d47ae2f67f'
 }
 
 let auth = {
-	init() {
-		return new Promise((res, rej) => {
-			Apis.instance('ws://144.217.15.182:11011', true).init_promise.then(() => {
-				ChainStore.init().then(() => {
-					res()
-				}, err => rej(err))
-			})
-		})
-	},
+  init() {
+    return new Promise((res, rej) => {
+      Apis.instance('ws://144.217.15.182:11011', true).init_promise.then(() => {
+        ChainStore.init().then(() => {
+          res()
+        }, err => rej(err))
+      })
+    })
+  },
 
-	login(accountName, password) {
-		FetchChain('getAccount', [accountName]).then(res => {
-			let [account] = res
+  login(accountName, password) {
+    FetchChain('getAccount', [accountName]).then(res => {
+      let [account] = res
 
-			console.log(ChainConfig.address_prefix)
-			let success = Login.checkKeys({
-				accountName: accountName,
-				password: password,
-				auths: {
-					active: account.get('active').toJS().key_auths
-				}
-			})
+      let success = Login.checkKeys({
+        accountName: accountName,
+        password: password,
+        auths: {
+          active: account.get('active').toJS().key_auths
+        }
+      })
 
-			if (success) {
-				console.log('success')
-			} else {
-				console.log('err')
-			}
-		}, () => {
-			console.info('Пользователь не существует')
-		})
-	}
+      if (success) {
+        console.log('success')
+      } else {
+        console.log('err')
+      }
+    }, () => {
+      console.info('Пользователь не существует')
+    })
+  }
 }
 
 auth.init().then(() => {
-	auth.login('accName', 'pass')
+  auth.login('accName', 'password')
 })
